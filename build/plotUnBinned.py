c69b69f217123827aeff498985555ad9376fdff3
vcroft <vincent.croft@cern.ch>
2017-11-03 11:21:01 -0400

    Getting ready to merge

--- build/plotUnBinned.py
+++ build/plotUnBinned.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-times = pd.read_csv('testUnBinned01.csv',skiprows=3)
+times = pd.read_csv('testUnBinned03.csv',skiprows=3)
 times = times[['name','real_time','cpu_time']]
 
 times['name'] = times['name'].map(lambda x: x.replace('/iterations:12/real_time',''))
@@ -26,13 +26,15 @@ plottimes['real_time']=plottimes['real_time']*10**-9
 plottimes.rename(columns={'real_time':'time'},inplace = True)
 
 print plottimes
+types = plottimes[plottimes['type']=='real']
+h = sns.factorplot(x="events", y="time",hue="ncpus",col="process", data = types, type='bar')
+h.set_axis_labels("Number of Events", "Time (Seconds)").despine(left=True).set_xticklabels(rotation=45)
 times = plottimes[plottimes['type']!='cpu']
-
+times = times[times['process']=='BDecayWithMixing']
 g = sns.factorplot(x="events", y="time",hue="ncpus",col="type", data = times, type='bar')
 g.set_axis_labels("Number of Events", "Time (Seconds)").despine(left=True).set_xticklabels(rotation=45)
 
 cputimes = plottimes[plottimes['ncpus']>1]
-
 f = sns.factorplot(x="events", y="time",hue="ncpus",col="type", data = cputimes[cputimes['type']=='cpu'], type='bar')
 f.set_axis_labels("Number of Events", "Time (Seconds)").despine(left=True).set_xticklabels(rotation=45)
 
